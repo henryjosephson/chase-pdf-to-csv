7edf21fe6ddab8016bf122c50080ec7d781bde47
henryjosephson <131294008+henryjosephson@users.noreply.github.com>
2024-09-30 21:05:42 -0500

    added transaction amount, takes in directory of pdfs

--- bank statement to csv.py
+++ bank statement to csv.py
@@ -1,59 +1,74 @@
-from pathlib import Path
 import re
-import pdfplumber
+from pathlib import Path
+
 import pandas as pd
+import pdfplumber
+
+transaction_date = re.compile(r"^\d+\/\d\d")  # handle dates in MM/DD format
+transaction_amount = re.compile(
+    r"(\d+\,)?\d+\.\d\d$"
+)  # handle amounts over 1000, including commas
 
 pdfs = []
 for pdf in Path("../Bank Statements/Chase College Checking/pdfs/").glob("*.pdf"):
     pdfs.append(pdf)
+pdfs = sorted(pdfs)
 
+for file in pdfs:
+    with open(file, "rb") as f:
 
-transaction_date = re.compile(r'^\d+\/\d\d') #handle dates in MM/DD format
-transaction_amount = re.compile(r'(\d+\,)?\d+\.\d\d$') #handle amounts over 1000, including commas
+        with pdfplumber.open(f) as pdf:
+            print(file)
 
-dates = []
-merchants = []
-amounts = []
+            dates = []
+            merchants = []
+            amounts = []
+            balances = []
 
-openfile = sorted(pdfs)[0]
+            first_page = pdf.pages[0]
+            lines = first_page.extract_text().split("\n")
+            year = None
+            while not year:
+                for line in lines:
+                    if "through" in line:
+                        startdate, enddate = line.split("through")
+                        year = startdate.split()[-1]
+                        break
 
-with pdfplumber.open(openfile) as pdf:
-    pages = pdf.pages
+            for page in pdf.pages:
+                text = page.extract_text()
 
-    first_page = pdf.pages[0]
-    lines = first_page.extract_text().split("\n")
-    year = None
-    while not year:
-        for line in lines:
-            if "through" in line:
-                year = re.search(r"\d{4}", line).group()
+                month_for_prev_transaction = None
 
-    for page in pdf.pages:
-        text = page.extract_text()
+                for line in text.split("\n"):
+                    has_date = transaction_date.search(
+                        line
+                    )  # if line starts with a date of format MM/DD
+                    actual_transaction = transaction_amount.search(
+                        line
+                    )  # if line ends with a dollar amount of format DD+.DD
 
-        month_for_prev_transaction = None
+                    if has_date:
+                        if actual_transaction:
+                            # print(line.split())
+                            month = has_date.group().split("/")[0]
+                            day = has_date.group().split("/")[1]
 
-        for line in text.split('\n'):
-            has_date = transaction_date.search(line) #if line starts with a date of format MM/DD
-            actual_transaction = transaction_amount.search(line) #if line ends with a dollar amount of format DD+.DD
-            print(line)
-            if has_date:
-                if actual_transaction:
+                            if month == "01" and month_for_prev_transaction == "12":
+                                year = str(int(year) + 1)
 
-                    month = has_date.group().split('/')[0]
-                    day = has_date.group().split('/')[1]
+                            dates += [f"{year}-{month}-{day}"]
+                            merchants.append(" ".join(line.split()[1:-2]))
+                            amounts += [line.split()[-2]]
+                            balances += [actual_transaction.group()]
 
-                    if month == '01' and month_for_prev_transaction == '12':
-                        year = str(int(year) + 1)
+                            month_for_prev_transaction = month
 
-                    dates += [f"{year}-{month}-{day}"]
-                    merchants.append(line[5:-5])
-                    amounts += [actual_transaction.group()]
+            zipped = list(zip(dates, merchants, amounts, balances))
+            df = pd.DataFrame(
+                zipped, columns=["Date", "Description", "Amount", "Balance"]
+            )
 
-                    month_for_prev_transaction = month
-
-#print (dates, merchants, amounts)
-zipped = list(zip(dates, merchants, amounts))
-df = pd.DataFrame(zipped, columns=['Date', 'Description', 'Amount'])
-
-df.Description.iloc[0]
+            df.to_csv(
+                f"../Bank Statements/Chase College Checking/csvs/{year}-{month}.csv"
+            )
